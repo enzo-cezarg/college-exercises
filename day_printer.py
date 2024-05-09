5cde3c24274f43800bec7904dd950c2e4a9164f8
Enzo <enzoocezargarcia@gmail.com>
2024-05-09 16:16:58 -0300

    refactor: day-printer-elif

--- day_printer.py
+++ day_printer.py
@@ -1,24 +1,20 @@
 # Entrada de dados
 dia = int(input('Informe o dia da semana como um valor inteiro (1 a 7): '))
 
-# Verificação do número digitado e atribuição do dia correspondente
+# Verificação do número digitado e saída de dados
 if not(dia >=1 and dia <= 7):
     print('Valor Inválido! Fora do intervalo de 1 a 7')
+elif dia == 1:
+    print('Dia da semana: Domingo')
+elif dia == 2:
+    print('Dia da semana: Segunda-feira')
+elif dia == 3:
+    print('Dia da semana: Terça-feira')
+elif dia == 4:
+    print('Dia da semana: Quarta-feira')
+elif dia == 5:
+    print('Dia da semana: Quinta-feira')
+elif dia == 6:
+    print('Dia da semana: Sexta-feira')
 else:
-    if dia == 1:
-        diaSemana = 'Domingo'
-    if dia == 2:
-        diaSemana = 'Segunda-feira'
-    if dia == 3:
-        diaSemana = 'Terça-feira'
-    if dia == 4:
-        diaSemana = 'Quarta-feira'
-    if dia == 5:
-        diaSemana = 'Quinta-feira'
-    if dia == 6:
-        diaSemana = 'Sexta-feira'
-    if dia == 7:
-        diaSemana = 'Sábado'
-
-# Saída de dados
-    print('Dia da semana:', diaSemana)
+    print('Dia da semana: Sábado')
